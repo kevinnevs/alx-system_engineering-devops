ce944aaf70d9c994f99f0f9dc41894937fa637a0
kevinnevs <kevinnjuguna2@gmail.com>
2022-12-09 12:58:19 -0800

    fixin code

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # 2-execute_a_command.pp
 
 exec { 'killmenow':
-  command     => 'pkill -f "killmenow"',
-  path        => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command     => 'pkill -f killmenow',
+  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
