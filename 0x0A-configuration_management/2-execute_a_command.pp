a7c35663cff09f4cdf156db65318a8bc0bb420b4
kevinnevs <kevinnjuguna2@gmail.com>
2022-12-09 12:43:46 -0800

    initial commit

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# 2-execute_a_command.pp
+
+exec { 'killmenow':
+  command     => 'pkill -f killmenow',
+  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  refreshonly => true,
+}
