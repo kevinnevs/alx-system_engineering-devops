c6beead5fa0b609d2d530053abfad753512f1a22
kevinnevs <kevinnjuguna2@gmail.com>
2022-12-11 03:07:14 -0800

    fixing syntax

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # 2-execute_a_command.pp
 
 exec { 'killmenow':
-  command     => 'pkill -f killmenow',
-  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  command => 'pkill -f killmenow',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
