c1bae885e6f05ac10a7c5e7ccb23506d38a495b9
kevinnevs <kevinnjuguna2@gmail.com>
2023-01-31 18:45:43 +0000

    initial commit

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+recursive function that queries "Reddit API" & returns
+a list containing titles of all hot artciles for subreddit
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    headers = {'User-Agent' : 'CustomUserAgent'}
+    url = ("https://www.reddit.com/r/{}/hot.json".format(subreddit))
+    params = {'limit': 100, 'after': after}
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    
+    if response.status_code == 200:
+        data = response.json()
+        after = data.get('data').get('after')
+        hot_posts = data.get('data').get('children')
+        for post in hot_posts:
+            hot_list.append(post.get('data').get('title'))
+        if after:
+            recurse(subreddit, hot_list, after)
+        return hot_list
+    return None
